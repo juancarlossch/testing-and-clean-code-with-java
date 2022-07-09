e5d0ae1dfcecc8ab8ae22b2c722ef5015319d660
juancarlossch <juancarlossch6@gmail.com>
2022-07-09 12:51:24 -0500

    ft: Commit inicial

---
+++ src/main/java/com/curso/testindandcleancode/TestindandcleancodeApplication.java
@@ -0,0 +1,13 @@
+package com.curso.testindandcleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestindandcleancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestindandcleancodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/testindandcleancode/TestindandcleancodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.testindandcleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestindandcleancodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
